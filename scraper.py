eb382e83996f5918736933664ad71d060d570530
Paul M Furley <paul@paulfurley.com>
2018-06-08 16:24:26 +0100

    Update scraper code

--- scraper.py
+++ scraper.py
@@ -1,24 +1,158 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+#!/usr/bin/env python3
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import datetime
+import logging
+import os
+import sys
+import re
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+from os.path import abspath, dirname, join as pjoin
+
+import dataset
+import requests_cache
+import lxml.html
+
+ONE_HOUR = datetime.timedelta(hours=1)
+
+
+class RequestsWrapper():
+    def __init__(self):
+        self.session = requests_cache.core.CachedSession(expire_after=ONE_HOUR)
+
+    def get(self, url, *args, **kwargs):
+        return self.session.get(url, *args, **kwargs)
+
+
+def main(output_dir=None):
+    logging.basicConfig(level=logging.DEBUG)
+    output_dir = output_dir or abspath(pjoin(dirname(__file__), 'output'))
+    scraper = ICOPenaltyScraper(output_dir, RequestsWrapper())
+
+    db = dataset.connect('sqlite:///data.sqlite')
+    table = db['data']
+
+    for row in scraper.run():
+        table.upsert(row, ['url'])
+
+
+class ICOPenaltyScraper():
+    BASE_URL = 'https://ico.org.uk'
+    LIST_URL = '{}/action-weve-taken/enforcement/'.format(BASE_URL)  # noqa
+
+    XPATH_LIST_PAGE_LINK = '//a[contains(@href, "/action-weve-taken/enforcement/")]'  # noqa
+    XPATH_PDF_LINK = "//a[contains(@href, '/media/action-weve-taken') and contains(@href, '.pdf')]"  # noqa
+    XPATH_DATE = "//dt[contains(text(), 'Date')]/following-sibling::dd[1]"  # noqa
+
+    def __init__(self, output_dir, requests_like_object):
+        self.output_dir = output_dir
+        self.http = requests_like_object
+        self.penalty_pages = None
+        self.actions = None
+
+    def run(self):
+        self.mkdir_p(pjoin(self.output_dir, 'pdfs'))
+        self.parse_list_page()
+
+        for url in self.penalty_pages:
+            yield self.parse_extra_data_from_penalty_page(url)
+
+    @staticmethod
+    def mkdir_p(directory):
+        if not os.path.isdir(directory):
+            os.makedirs(directory)
+        return directory
+
+    def parse_list_page(self):
+        root = self._get_as_lxml(self.LIST_URL)
+
+        self.penalty_pages = [
+            self._expand_href(a.attrib['href']) for a in root.xpath(
+                self.XPATH_LIST_PAGE_LINK)
+        ]
+
+    def parse_extra_data_from_penalty_page(self, url):
+        """
+        Return a PDF() object for PDF URL linked in the penalty page.
+        """
+        root = self._get_as_lxml(url)
+        pdf_url = self._parse_pdf_url(root, url)
+
+        return {
+            'url': url,
+            'pdf_id': self._parse_id(pdf_url),
+            'pdf_url': pdf_url,
+            'type': self._parse_type(pdf_url),
+            'date': self._parse_date(root),
+            'title': self._parse_title(root),
+        }
+
+    def _parse_pdf_url(self, lxml_root, url):
+        a_tags = lxml_root.xpath(self.XPATH_PDF_LINK)
+
+        if len(a_tags) == 0:
+            logging.info("Couldn't find a PDF on page {}".format(url))
+
+        elif len(a_tags) == 1:
+            return self._expand_href(a_tags[0].attrib['href'])
+
+        else:
+            raise RuntimeError('Multiple PDF links: on page {} {}'.format(
+                url, a_tags))
+
+    def _parse_title(self, lxml_root):
+        h1s = lxml_root.xpath('//h1')
+        if len(h1s) == 1:
+            return h1s[0].text_content().strip()
+
+    def _parse_date(self, lxml_root):
+        def parse(date_string):
+            "e.g. 21 December 2017"
+            return datetime.datetime.strptime(
+                date_string, '%d %B %Y'
+            ).date().isoformat()
+
+        dates = lxml_root.xpath(self.XPATH_DATE)
+        if len(dates) == 1:
+            return parse(dates[0].text_content().strip())
+
+    def _parse_id(self, pdf_url):
+        if pdf_url is None:
+            return None
+
+        match = re.search('\/(?P<id>\d+)\/', pdf_url)
+        if match:
+            return match.group('id')
+
+    def _parse_type(self, pdf_url):
+        if pdf_url is None:
+            return None
+
+        match = re.search('\/action-weve-taken\/(?P<type>.+?)\/', pdf_url)
+        if match:
+            type_slug = match.group('type')
+
+            return {
+                'enforcement-notices': 'enforcement-notice',
+                'mpns': 'monetary-penalty',
+                'undertakings': 'undertaking',
+            }.get(type_slug, None)
+
+    def _get_as_lxml(self, url):
+        logging.info(url)
+        response = self.http.get(url)
+        response.raise_for_status()
+
+        if response.status_code == 301:
+            raise RuntimeError(response.headers)
+
+        return lxml.html.fromstring(response.text)
+
+    def _expand_href(self, href):
+        if href.startswith('/'):  # not complete
+            return '{}{}'.format(self.BASE_URL, href)
+        else:
+            return href
+
+
+if __name__ == '__main__':
+    main(*sys.argv[1:])
